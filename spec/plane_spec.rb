ec59b02edae320df9d622b69a1e7a4d9c735c60c
Sarah Young <y.sarahc@gmail.com>
2014-03-07 15:59:05 +0000

    Added tests for plane to know if it's flying or landed

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,10 +4,14 @@ describe Plane do
 
 	let (:plane) { Plane.new } 
 
-	# it "should be able to fly" do
-	# 	expect(plane.fly?).to be_true
-	# end
+	it "should know if it's flying" do
+		expect(plane.flying?).to be_true
+	end
 
+	it "should know if it has landed" do
+		expect(plane.landed?). to be_true
+	end
+	
 	it "should be able to land" do
 		expect(plane.can_land?).to be_true
 	end
