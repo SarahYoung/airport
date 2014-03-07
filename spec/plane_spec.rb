d485df306f85d19f31881680cb1d211efc884997
Sarah Young <y.sarahc@gmail.com>
2014-03-07 15:01:10 +0000

    Refactored initialization

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,18 +2,17 @@ require_relative "../lib/plane"
 
 describe Plane do 
 
-	it "should be able to fly" do
-		plane = Plane.new
-		expect(plane.fly?).to be_true
-	end
+	let (:plane) { Plane.new } 
+
+	# it "should be able to fly" do
+	# 	expect(plane.fly?).to be_true
+	# end
 
 	it "should be able to land" do
-		plane = Plane.new
 		expect(plane.can_land?).to be_true
 	end
 
 	it "should be able to take off" do
-		plane = Plane.new
 		expect(plane.can_take_off?).to be_true
 	end
 
