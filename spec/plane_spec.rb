bee8ffc6acc610a4e8c82f0dda4b7429c817a2c5
Sarah Young <y.sarahc@gmail.com>
2014-03-09 12:03:25 +0000

    Created tests to see if plane can fly and land

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,16 +4,28 @@ require_relative "../lib/plane"
 describe Plane do
 	# this is a specific feature
 	# that we expect to be present
-	it "should not be able to fly after we create it" do
+	it "should not be flying after we create it" do
 		plane = Plane.new # initalizing a new object of plane class	
 		# expect an instance of this class to have
 		# a method "flying?" that should return false
-		expect(plane).not_to(be_able_to_fly)
+		expect(plane).not_to(be_flying)
 	end
 
-	it "should be able to fly after we create it" do
+	it "should fly after we create it" do
+		# creating a new instance of plane
 		plane = Plane.new
+		# calling the method can_fly on the instance plane
 		plane.can_fly
-		expect(plane).to(be_able_to_fly)
+		# expecting the method flying? to return true
+		expect(plane).to(be_flying)
+	end
+
+	it "should be able to land" do
+		# creating a new instance of plane
+		plane = Plane.new
+		# calling the method can_land on this instance 
+		plane.can_land
+		# expecting the method flying? to be false
+		expect(plane).not_to(be_flying) 
 	end
 end
