d317cb872f88791dfa58677d943b276044a7178e
Sarah Young <y.sarahc@gmail.com>
2014-03-09 15:25:31 +0000

    Added methods to add/deduct planes from @planes array

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,22 @@
 class Airport
 	
 	def initialize
+		# initializes @planes to equal an array
 		@planes = []
 	end
 	
 	def plane_count
+		# the method plane_count counts the planes in the array
 		@planes.count
 	end
 
 	def dock(plane)
+		# puts the value/argument plane into the @planes array
 		@planes << plane
+	end
+
+	def release(plane)
+		# deletes (takes) a plane from the @planes array
+		@planes.delete(plane)
 	end
 end
