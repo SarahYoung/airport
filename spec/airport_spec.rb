eed9af9a6d762883931dbac1e4d1987af2d942a6
Sarah Young <y.sarahc@gmail.com>
2014-03-09 14:14:16 +0000

    Created tests to see if airport can dock planes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,14 @@
+require_relative "../lib/airport"
+require_relative "../lib/plane"
+
 describe Airport do
 	it "should allow a plane to land" do
-	plane = Plane.new
-	airport = Airport.new
-	# we expect the airport to have 0 planes
-	expect(airport.plane_count).to eq(0)
-	# let's land a plan into the station
+		plane = Plane.new
+		airport = Airport.new
+		# we expect the airport to have 0 planes
+		expect(airport.plane_count).to eq(0)
+		# let's land a plane at the airport
+		airport.dock(plane)
+		expect(airport.plane_count).to eq(1)
+	end
 end
