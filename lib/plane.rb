cf5fab4d494b4eb796b3990da1470d1d1198cb79
Sarah Young <y.sarahc@gmail.com>
2014-03-09 21:58:16 +0000

    Added methods to fly, land, & take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,31 +1,19 @@
 class Plane
 	
-	# the initialize method is always called when I create a 
-	# new plane class by typing Plane.new
 	def initialize
-		can_land
-		# we've initialized the plane to automatically not be flying
-	end
-	# all instance variables begin with @
-	# this must be an instance variable b/c 
-	# we'll need it in other variables
-	def flying?
-		@flying
-		# defining/setting the method able_to_fly as false (i.e, can't fly), see line 6
+		@flying = true
 	end
 
 	def can_fly
-		@flying = true
-		# setting the method can_fly to be true (i.e, the plane can fly)
+		@flying
 	end
 
-	def can_land
-		@flying =false
-		# value returns false (which is what we expect)
-		# b/c we've set @flying to false, and if the plane has landed it shouldn't be flying
+	def land
+		@flying = false
 	end
 
 	def can_take_off
-		can_fly
+		@flying
 	end
+
 end
