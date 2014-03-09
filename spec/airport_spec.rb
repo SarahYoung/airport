af640c1866b13ac583a92ff6785eccb9ad335008
Sarah Young <y.sarahc@gmail.com>
2014-03-09 13:53:45 +0000

    Updated whole airport repo

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,8 @@
-require_relative "../lib/airport"
-
 describe Airport do
-
-	let(:airport) { Airport.new }
-
-	it "should be able to clear planes for landing" do
-		expect(airport.clear_landing).to be_true
-	end
-
-	it "should be able to clear planes for take-off" do
-		expect(airport.clear_take_off).to be_true
-	end
+	it "should allow a plane to land" do
+	plane = Plane.new
+	airport = Airport.new
+	# we expect the airport to have 0 planes
+	expect(airport.plane_count).to eq(0)
+	# let's land a plan into the station
 end
