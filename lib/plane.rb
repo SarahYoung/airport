d40339db676f156df4737e6d49fe42cf3a932b84
Sarah Young <y.sarahc@gmail.com>
2014-03-09 12:01:00 +0000

    Created methods for plane to fly & land

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,19 +3,25 @@ class Plane
 	# the initialize method is always called when I create a 
 	# new plane class by typing Plane.new
 	def initialize
-		@able_to_fly = false
-		# we've initialized the plane to automatically not be able to fly
+		@flying = false
+		# we've initialized the plane to automatically not be flying
 	end
 	# all instance variables begin with @
 	# this must be an instance variable b/c 
 	# we'll need it in other variables
-	def able_to_fly?
-		@able_to_fly
-		# defining/setting the method able_to_fly as false (i.e, can't fly)
+	def flying?
+		@flying
+		# defining/setting the method able_to_fly as false (i.e, can't fly), see line 6
 	end
 
 	def can_fly
-		@able_to_fly = true
+		@flying = true
 		# setting the method can_fly to be true (i.e, the plane can fly)
+	end
+
+	def can_land
+		@flying =false
+		# value returns false (which is what we expect)
+		# b/c we've set @flying to false, and if the plane has landed it shouldn't be flying
 	end
 end
